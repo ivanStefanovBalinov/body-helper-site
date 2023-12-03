05c61828389176e69d1b377277314659e37f8873
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-12-03 21:32:11 +0200

    Setup Change pass router

--- body-helper-web/db/models/Users.model.js
+++ body-helper-web/db/models/Users.model.js
@@ -51,6 +51,10 @@ UserSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
