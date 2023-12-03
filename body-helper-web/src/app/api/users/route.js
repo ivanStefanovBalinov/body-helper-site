bd1b4baa915c32491066e91a0402d920956b015f
ivanStefanovBalinov <116738791+ivanStefanovBalinov@users.noreply.github.com>
2023-12-03 20:55:13 +0200

    Setup all CRUDS for User

--- body-helper-web/src/app/api/users/route.js
+++ body-helper-web/src/app/api/users/route.js
@@ -19,11 +19,3 @@ export async function POST(request) {
   }
   return NextResponse.json({ user, success: true }, { status: 201 });
 }
-
-export async function PUT(request) {
-  return NextResponse.json({ message: "UPDATE USER" }, { status: 201 });
-}
-
-export async function DELETE(request) {
-  return NextResponse.json({ message: "DELETE USER" });
-}
